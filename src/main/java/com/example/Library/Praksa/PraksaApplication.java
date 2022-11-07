48f2b204d531a0720ef0c4def48f467a3c114119
marko adamovic <markoadamovic@markos-MacBook-Pro.local>
2022-11-07 11:55:38 +0100

    main commit

---
+++ src/main/java/com/example/Library/Praksa/PraksaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Library.Praksa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraksaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraksaApplication.class, args);
+	}
+
+}
