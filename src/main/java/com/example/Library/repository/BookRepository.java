1d64e642f51d30606f22f4500f907f43e84d45af
marko adamovic <markoadamovic@markos-MacBook-Pro.local>
2022-11-09 09:25:54 +0100

    added space before/after parametars

--- src/main/java/com/example/Library/repository/BookRepository.java
+++ src/main/java/com/example/Library/repository/BookRepository.java
@@ -4,6 +4,6 @@ import com.example.Library.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository <Book, Long> {
 
 }
